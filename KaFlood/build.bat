b5459b98468eb14405941fbe304dd45fea6bf7f2
Roeppli <108693445+DonRoeppli@users.noreply.github.com>
2026-05-26 23:30:15 +0200

    Add files via upload

--- KaFlood/build.bat
+++ KaFlood/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-C:\Users\Nico\AppData\Local\Programs\Python\Python313\python.exe -m PyInstaller --onefile --noconsole ^
+py -m PyInstaller --onefile --noconsole ^
 --icon="icon.ico" ^
 --name="KaFlood" ^
 --add-data "icon.ico;." ^
